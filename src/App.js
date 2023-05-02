180d87b21755d385567492c0d204a5c48af9f30b
Ulissesortega <Ulisses.ortega@hotmail.com>
2023-05-01 22:37:41 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/MyNavbar";
-import Landing from "./Components/Landing";
+import Landing from "./Components/Landing.js";
+import About from './Components/About.js'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing/>}/>
+        <Route path='/' element={<Landing/>}/>        
+        <Route path='/About' element={<About />}/>       
       </Routes>
     </BrowserRouter>
   );
--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -2,18 +2,19 @@ import React from 'react'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container, Row, Col, Navbar, Nav} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function MyNavbar() {
   return (
     <div>      
       <Navbar bg="" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Brand href="#home" as={Link} to='/'>Home</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#About">About</Nav.Link>
-            <Nav.Link href="#Skills">Skills</Nav.Link>
-            <Nav.Link href="#Projects">Projects</Nav.Link>
-            <Nav.Link href="#Contact">Contact</Nav.Link>
+            <Nav.Link href="#About" as={Link} to='About'>About</Nav.Link>
+            <Nav.Link href="#Skills" as={Link} to='Skills'>Skills</Nav.Link>
+            <Nav.Link href="#Projects" as={Link} to='Projects'>Projects</Nav.Link>
+            <Nav.Link href="#Contact" as={Link} to='Contact'>Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
