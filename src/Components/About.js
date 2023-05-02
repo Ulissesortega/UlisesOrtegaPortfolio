f951f27bb8aa6f3423db38e615c86699484cf4bd
Ulissesortega <Ulisses.ortega@hotmail.com>
2023-05-01 23:04:44 -0700

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./MyNavbar";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { NavLink, useLocation } from 'react-router-dom';
+
 
 export default function About() {
+
+  const { pathname } = useLocation();
+
   return (
-    <div>About</div>
-  )
+    <div className="bgimage">
+      <Navbar />
+    </div>
+  );
 }
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Link } from 'react-router-dom'
-import Navbar from './MyNavbar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./MyNavbar";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function Landing() {
   return (
-    <div className='bgimage'>
-      <Navbar/>            
+    <div className="bgimage">
+      <Navbar />
+      <Container>
+        <h1>Ulises Ortega</h1>
+      </Container>
     </div>
-  )
+  );
 }
-
