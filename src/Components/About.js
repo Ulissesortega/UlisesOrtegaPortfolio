fd891fcb4b2cee1c7866cff7e2e99e3b6352eb1d
UlissesOrtega <51983586+Ulissesortega@users.noreply.github.com>
2023-05-06 20:17:40 -0700

    Updated About

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,12 +12,30 @@ export default function About() {
   return (
     <div className="bgimage">
       <Navbar />
-      <Row>
-        <Col>
-          <p>Skills Component</p>
-          
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col>
+            <p className="title-format title-location">About Me!</p>
+            <p className="under-title under-title-loc text-white">I'm a Front-End Web Developer with a background in 3D design, 
+              in addition to technical support for computers software and hardware.<br/><br/>
+              My previous years of experience have giving me a strong foundation for Web Development,<br/>
+              I gratuated from <span className="under-title">CodeStack Academy's immersive Web Development program.</span>
+              <br/>
+              <br/>
+              I'm pasitionate about about coding and bringing creatiave solutions through code, <br/> As challenging as it could be, I love the learning challenge!   
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Link to="/About">
+              <Button className="rounded-pill under-title-loc font-italic">About Me</Button>
+            </Link>
+          </Col>
+        </Row>
+
+      </Container>
     </div>
   );
 }
