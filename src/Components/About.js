b6d63f70683ceed7cf6cd5fca3fe14d68702c18a
UlissesOrtega <51983586+Ulissesortega@users.noreply.github.com>
2023-05-09 23:11:01 -0700

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,20 +17,20 @@ export default function About() {
           <Col>
             <p className="title-format title-location">About Me!</p>
             <p className="under-title under-title-loc text-white">I'm a Front-End Web Developer with a background in 3D design, 
-              in addition to technical support for computers software and hardware.<br/><br/>
+              technical support for computers<br/> software and hardware.<br/><br/>
               My previous years of experience have giving me a strong foundation for Web Development,<br/>
               I gratuated from <span className="under-title">CodeStack Academy's immersive Web Development program.</span>
               <br/>
               <br/>
-              I'm pasitionate about about coding and bringing creatiave solutions through code, <br/> As challenging as it could be, I love the learning challenge!   
+              I'm pasitionate about coding and bringing creatiave solutions through code, <br/> As challenging as it could be, I love the learning challenge!   
             </p>
           </Col>
         </Row>
 
         <Row>
           <Col>
-            <Link to="/About">
-              <Button className="rounded-pill under-title-loc font-italic">About Me</Button>
+            <Link to="/Skills">
+              <Button className="rounded-pill under-title-loc font-italic">My Skills</Button>
             </Link>
           </Col>
         </Row>
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -9,12 +9,52 @@ import { NavLink, useLocation } from 'react-router-dom';
 export default function Skills() {
   return (
     <div className="bgimage">
-      <Navbar/>
-      <Row>
-        <Col>
-          <p>About Component</p>
-        </Col>
-      </Row>
-    </div>    
+      <Navbar />
+      <Container>
+        <Row>
+          <Col>
+            <p className="title-format title-location">Skills</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col lg={2}>
+            <a href="https://www.w3schools.com/html/" target="_blank">
+              <img className='imgloc br' src={require('../Assets/html.png')} width="100" height="100" />
+            </a>
+          </Col>
+
+          <Col lg={2}>
+            <img className='imgloc br' src={require('../Assets/css.png')} width="100" height="100" />
+          </Col>
+
+          <Col lg={2}>
+            <img className='imgloc br' src={require('../Assets/bootstrap.png')} width="100" height="100" />
+          </Col>
+
+          <Col lg={2}>
+            <img className='imgloc br' src={require('../Assets/tailwind.png')} width="100" height="100" />
+          </Col>
+
+          <Col lg={2}>
+            <img className='imgloc br' src={require('../Assets/Javascript.png')} width="100" height="100" />
+          </Col>
+
+
+
+        </Row>
+
+
+
+        <Row className='mt-5'>
+          <Col>
+            <Link to="/About">
+              <Button className="rounded-pill under-title-loc font-italic">About Me</Button>
+            </Link>
+          </Col>
+        </Row>
+
+      </Container>
+    </div>
   )
 }
